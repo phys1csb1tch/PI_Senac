ec331c5ab7a266acaa35829a91b1f9717de1e05d
Mailan Franco <36058994+thesckurtt@users.noreply.github.com>
2024-12-03 15:25:07 -0300

    updates

--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,12 @@ import { Usuario } from "./Classes/Usuario";
 import { Genero } from "./Classes/Genero";
 import { Livros } from "./Classes/Livros";
 import promptSync from 'prompt-sync';
+import { UserScreen } from "./screens/UserScreen";
 const prompt = promptSync();
 // let login: boolean = false;
 
 // ----------- INSTANCIANDO USUARIOS -------------------
-const user1 = new Usuario ("Jade", "038", "ali", "123")
+const user1 = new Usuario("Jade", "038", "ali", "123")
 const admin1 = new Admin("admin", "admin")
 
 
@@ -31,7 +32,7 @@ const livro2 = new Livros("O Senhor dos Anéis", "J.R.R. Tolkien", new Date(1954
 const livro3 = new Livros("Drácula", "Bram Stoker", new Date(1897, 4, 26), "", "", 500, [generoTerror]);
 const livro4 = new Livros("Orgulho e Preconceito", "Jane Austen", new Date(1813, 0, 28), "", "", 432, [generoRomance]);
 const livro5 = new Livros("Harry Potter e a Pedra Filosofal", "J.K. Rowling", new Date(1997, 5, 26), "", "", 223, [generoFantasia]);
-const livro6 = new Livros("O Código Da Vinci", "Dan Brown", new Date(2003, 2, 18),"", "", 689, [generoPolicial]);
+const livro6 = new Livros("O Código Da Vinci", "Dan Brown", new Date(2003, 2, 18), "", "", 689, [generoPolicial]);
 const livro7 = new Livros("O Pequeno Príncipe", "Antoine de Saint-Exupéry", new Date(1943, 3, 6), "", "", 96, [generoAventura]);
 const livro8 = new Livros("O Diário de Anne Frank", "Anne Frank", new Date(1947, 5, 25), "", "", 283, [generoHistoria]);
 const livro9 = new Livros("Steve Jobs", "Walter Isaacson", new Date(2011, 9, 24), "", "", 656, [generoBiografia]);
@@ -40,7 +41,10 @@ const livro11 = new Livros("A Comédia Humana", "Honoré de Balzac", new Date(18
 const livro12 = new Livros("A Cabana", "William P. Young", new Date(2007, 2, 1), "", "", 256, [generoDrama]);
 
 
+console.clear()
+
 /*
+
 
 
 IDEIA: SEPARAR OS MENUS VISITANTE, USUARIO E ADMIN EM ARQUIVOS DIFERENTES
@@ -83,80 +87,97 @@ IDEIA: SEPARAR OS MENUS VISITANTE, USUARIO E ADMIN EM ARQUIVOS DIFERENTES
 
 // ----------------------- MENU --------------------------
 
-let controle=true;
-while (controle){
-    console.log("Bem vindo ao BookMatch!")
-    console.log("1. Login")
-    console.log("2. Listar Acervo")
-    console.log("3. Buscar no acervo")
-    console.log("4. Novo cadastro")
-    console.log("5. Sair")
-    let opcao = Number(prompt("Escolha uma opção: "))
-    let again= "1"
+let controle = true;
+let logado = false;
 
-    switch (opcao){
+while (controle) {
+    console.clear()
+
+
+    if (logado) {
+        console.log("Bem vindo ao BookMatch!")
+        console.log("2. Listar Acervo")
+        console.log("3. Buscar no acervo")
+        console.log("5. Sair")
+    } else {
+        console.log("Bem vindo ao BookMatch!")
+        console.log("1. Login")
+        console.log("2. Listar Acervo")
+        console.log("3. Buscar no acervo")
+        console.log("4. Novo cadastro")
+        console.log("5. Sair")
+    }
+    let opcao = Number(prompt("Escolha uma opção: "))
+    let again = "1"
+
+    switch (opcao) {
         case 1: // -------------------- LOGIN ----------------------   
-            while(again){
-                const user = prompt (`Entre com o nome de Usuário: `);
+            while (again) {
+                const user = prompt(`Entre com o nome de Usuário: `);
                 const usuarioEncontrado = Usuario.consultarUsuario(user)
-                if (usuarioEncontrado){
-                    const password = prompt (`Entre com a senha para o Usuário '${usuarioEncontrado.nome}': `)
-                    if (password === usuarioEncontrado.senha){
-                    console.log(`Bem vinde, ${usuarioEncontrado.nome}`)
-                    // login = true;
-                    break; 
+                if (usuarioEncontrado) {
+                    const password = prompt(`Entre com a senha para o Usuário '${usuarioEncontrado.nome}': `)
+                    if (password === usuarioEncontrado.senha) {
+                        console.log(`Bem vinde, ${usuarioEncontrado.nome}`)
+                        // login = true;
+                        UserScreen();
+                        prompt("Aperte qualquer tecla para continuar...")
+                        logado = true;
+                        break;
                     } else {
                         console.error("Senha incorreta.")
-                        again = prompt (`Deseja tentar novamente? 1: Sim | 2: Não`)
-                        if (again ==="2"){ break;}
+                        again = prompt(`Deseja tentar novamente? 1: Sim | 2: Não`)
+                        if (again === "2") { break; }
                     }
-                    
+
                 } else {
                     console.error("Usuário não encontrado.")
-                    again = prompt (`Deseja tentar novamente? 1: Sim | 2: Não`)
-                        if (again ==="2"){break}
+                    again = prompt(`Deseja tentar novamente? 1: Sim | 2: Não`)
+                    if (again === "2") { break }
                 }
-                }
+            }
             break;
         case 2: // ----------- listar acervo ----------------
             console.log("Listando acervo: ")
 
             Livros.listarLivros();
-                
+            prompt("Aperte enter para continuar: ");
             break;
         case 3: // ------------ consultar no acervo ---------
-            while (again){   // aqui ta dando treta na segunda entrada do loop em diante | Fé
-                const keyword = prompt ("Entre com uma palavra-chave para pesquisa: ")
+            while (again) {
+                const keyword = prompt("Entre com uma palavra-chave para pesquisa: ")
                 let encontrada = false
-                for (let livro of Livros.listaLivros){
-                    const valoresLivro = Object.values(livro).map((v) => v.toString());
-                    if (valoresLivro.some((valor) => valor.includes(keyword))){
-                        console.log (`Correspondência encontrada!`)
+                for (let livro of Livros.listaLivros) {
+                    const valoresLivro = Object.values(livro).map((v) => v.toString().toLowerCase());
+                    if (valoresLivro.some((valor) => valor.includes(keyword))) {
+                        console.log(`Correspondência encontrada!`)
                         console.log(livro.exibir())
                         encontrada = true;
                     }
                 }
-                
-                if (!encontrada){
+
+                if (!encontrada) {
                     console.log("Correspondência não encontrada.")
                 }
-                {
-                    again = prompt (`Deseja buscar novamente? 1: Sim | 2: Não`)
-                        if (again ==="2"){break}
+
+
+                again = prompt(`Deseja buscar novamente? 1: Sim | 2: Não: `)
+                if (again === "2") {
+                    break
                 }
-                break;
+
             }
             break;
         case 4: // --------------- Cadastre-se ---------------
-            const novoNome = prompt ("Nome: ")
-            const novoCPF = prompt ("CPF: ")
-            const novoEndereco = prompt ("Endereço: ")
-            const nova_senha = prompt ("Senha: ")
+            const novoNome = prompt("Nome: ")
+            const novoCPF = prompt("CPF: ")
+            const novoEndereco = prompt("Endereço: ")
+            const nova_senha = prompt("Senha: ")
 
-            new Usuario (novoNome, novoCPF, novoEndereco, nova_senha)
+            new Usuario(novoNome, novoCPF, novoEndereco, nova_senha)
 
             break;
-        case 5: 
+        case 5:
             console.log("Saindo. Até mais!")
             controle = false;
             break;
